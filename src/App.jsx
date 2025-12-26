2bf877a4aefdd8e32b10eae9506abddc54d1b4a7
Anuj <anujnautiyal393@gmail.com>
2025-12-26 22:24:50 +0530

    feat: add beat sounds in the drum pads

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './App.css'
 import Drumpad from './components/Drumpad'
 
@@ -14,19 +14,19 @@ function App() {
             <div className='text-6xl font-bold my-3'>Kick & Snare</div>
             <div className='grid grid-cols-3 w-[80%] h-[55%]'>
               <div className="first">
-                <Drumpad content="7 Tom-1"/>
-                <Drumpad content="4 O-Hihat"/>
-                <Drumpad content="1 C-Hihat"/>
+                <Drumpad beat="Tom-1" pressedKey="7" audiosrc="src/assets/tom-1.mp3"/>
+                <Drumpad beat="O-Hihat" pressedKey="4" audiosrc="src/assets/O-hihat.mp3"/>
+                <Drumpad beat="C-Hihat" pressedKey="1" audiosrc="src/assets/C-hihat.mp3"/>
               </div>
               <div className="second">
-                <Drumpad content="8 C-Cymbal"/>
-                <Drumpad content="5 Snare"/>
-                <Drumpad content="2 KICK"/>
+                <Drumpad beat="C-Cymbal" pressedKey="8" audiosrc="src/assets/C-cymbal.mp3"/>
+                <Drumpad beat="Snare" pressedKey="5" audiosrc="src/assets/snare.mp3"/>
+                <Drumpad beat="Kick" pressedKey="2" audiosrc="src/assets/kick.mp3"/>
               </div>
               <div className="third">
-                <Drumpad content="9 Tom-2"/>
-                <Drumpad content="6 R-cymbal"/>
-                <Drumpad content="3 Floortom"/>
+                <Drumpad beat="Tom-2" pressedKey="9" audiosrc="src/assets/tom-2.mp3"/>
+                <Drumpad beat="R-cymbal" pressedKey="6" audiosrc="src/assets/R-cymbal.mp3"/>
+                <Drumpad beat="Floortom" pressedKey="3" audiosrc="src/assets/floortom.mp3"/>
               </div>
             </div>
           </div>
--- src/components/Drumpad.jsx
+++ src/components/Drumpad.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Drumpad = ({ content }) => {
-    const [number, text] = content.split(" ")
+const Drumpad = ({ beat, pressedKey, audiosrc }) => {
+
+    const playAudio = () => {
+      const audio = new Audio(audiosrc);
+      audio.currentTime = 0;
+      audio.play();
+    }
+
+    useEffect(() => {
+      const handleKeydown = (event) => {
+        if (event.key === pressedKey){
+            playAudio();
+        }
+      }
+      window.addEventListener('keydown', handleKeydown);
+      
+      return () => window.removeEventListener('keydown', handleKeydown);
+      
+    }, [pressedKey]);
+    
+    
     return (
         <div className='h-[33%] flex justify-center items-center'>
-            <button className='border-2 border-[#676060] cursor-pointer w-37.5 hover:scale-101 active:scale-99 py-1 px-5 rounded-2xl shadow-[8px_10px_25px_-3px_#464543] backdrop-blur-[2px]'>
+            <button onClick={playAudio} className='border-2 border-[#676060] cursor-pointer w-37.5 hover:scale-101 active:scale-99 py-1 px-5 rounded-2xl shadow-[8px_10px_25px_-3px_#464543] backdrop-blur-[2px]'>
                 <div className='flex flex-col justify-center items-center'>
-                    <span className='text-3xl font-bold number text-shadow-sm text-shadow-amber-200'>{number}</span>
-                    <span className='text-xl font-semibold text-yellow-400'>{text}</span>
+                    <span className='text-3xl font-bold number text-shadow-sm text-shadow-amber-200'>{pressedKey}</span>
+                    <span className='text-xl font-semibold text-yellow-400'>{beat}</span>
                 </div>
             </button>
         </div>
