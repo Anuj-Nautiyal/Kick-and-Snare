a92bb8417834a52479fe18a198573f041e402483
Anuj <anujnautiyal393@gmail.com>
2025-12-26 14:08:40 +0530

    Initial commit via vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-9xl bg-purple-500'>hello</h1>
+    </>
+  )
+}
+
+export default App
