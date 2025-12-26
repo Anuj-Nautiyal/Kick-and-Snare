c67738b55b53bbd9e028f8bb84293fa209ba0f1a
Anuj <anujnautiyal393@gmail.com>
2025-12-26 23:29:04 +0530

    feat: make the app responsive

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,25 @@ function App() {
       <main className='App relative w-screen h-screen overflow-hidden'>
         <div className='full-screen-bg fixed'></div>
 
-        <section className='hero relative flex justify-center items-center text-white border h-full w-full z-2'>
-          <div className='drum-machine-card flex flex-col items-center gap-40 w-[50vw] h-[80vh]'>
-            <div className='text-6xl font-bold my-3'>Kick & Snare</div>
-            <div className='grid grid-cols-3 w-[80%] h-[55%]'>
-              <div className="first">
-                <Drumpad beat="Tom-1" pressedKey="7" audiosrc="src/assets/tom-1.mp3"/>
-                <Drumpad beat="O-Hihat" pressedKey="4" audiosrc="src/assets/O-hihat.mp3"/>
-                <Drumpad beat="C-Hihat" pressedKey="1" audiosrc="src/assets/C-hihat.mp3"/>
+        <section className='relative flex justify-center items-center text-white h-full w-full z-10 p-4'>
+          <div className='drum-machine-card flex flex-col items-center justify-center gap-15  w-[95%] sm:w-[80%] md:w-[70%] lg:w-[50%] h-[90%] md:h-[80%]'>
+            <div className='text-3xl md:text-5xl lg:text-6xl font-bold my-3 text-center'>Kick & Snare</div>
+
+            <div className='grid grid-cols-3 w-[95%] md:w-[80%] h-[60%] gap-2'>
+              <div className="first flex flex-col justify-center md:justify-between h-full">
+                <Drumpad beat="Tom-1" pressedKey="7" audiosrc="src/assets/tom-1.mp3" />
+                <Drumpad beat="O-Hihat" pressedKey="4" audiosrc="src/assets/O-hihat.mp3" />
+                <Drumpad beat="C-Hihat" pressedKey="1" audiosrc="src/assets/C-hihat.mp3" />
               </div>
-              <div className="second">
-                <Drumpad beat="C-Cymbal" pressedKey="8" audiosrc="src/assets/C-cymbal.mp3"/>
-                <Drumpad beat="Snare" pressedKey="5" audiosrc="src/assets/snare.mp3"/>
-                <Drumpad beat="Kick" pressedKey="2" audiosrc="src/assets/kick.mp3"/>
+              <div className="second flex flex-col justify-center md:justify-between h-full">
+                <Drumpad beat="C-Cymbal" pressedKey="8" audiosrc="src/assets/C-cymbal.mp3" />
+                <Drumpad beat="Snare" pressedKey="5" audiosrc="src/assets/snare.mp3" />
+                <Drumpad beat="Kick" pressedKey="2" audiosrc="src/assets/kick.mp3" />
               </div>
-              <div className="third">
-                <Drumpad beat="Tom-2" pressedKey="9" audiosrc="src/assets/tom-2.mp3"/>
-                <Drumpad beat="R-cymbal" pressedKey="6" audiosrc="src/assets/R-cymbal.mp3"/>
-                <Drumpad beat="Floortom" pressedKey="3" audiosrc="src/assets/floortom.mp3"/>
+              <div className="third flex flex-col justify-center md:justify-between h-full">
+                <Drumpad beat="Tom-2" pressedKey="9" audiosrc="src/assets/tom-2.mp3" />
+                <Drumpad beat="R-cymbal" pressedKey="6" audiosrc="src/assets/R-cymbal.mp3" />
+                <Drumpad beat="Floortom" pressedKey="3" audiosrc="src/assets/floortom.mp3" />
               </div>
             </div>
           </div>
--- src/components/Drumpad.jsx
+++ src/components/Drumpad.jsx
@@ -22,11 +22,13 @@ const Drumpad = ({ beat, pressedKey, audiosrc }) => {
     
     
     return (
-        <div className='h-[33%] flex justify-center items-center'>
-            <button onClick={playAudio} className='border-2 border-[#676060] cursor-pointer w-37.5 hover:scale-101 active:scale-99 py-1 px-5 rounded-2xl shadow-[8px_10px_25px_-3px_#464543] backdrop-blur-[2px]'>
+        <div className='h-[25%] md:h-[30%] flex justify-center items-center'>
+            <button onClick={playAudio} 
+                    className='border-2 border-[#676060] cursor-pointer w-full max-w-20 sm:max-w-30 md:max-w-37.5 hover:scale-105 active:scale-95 py-2 md:py-4 px-1 rounded-xl md:rounded-2xl shadow-[5px_5px_15px_-3px_#464543] backdrop-blur-[2px] transition-transform'
+            >
                 <div className='flex flex-col justify-center items-center'>
-                    <span className='text-3xl font-bold number text-shadow-sm text-shadow-amber-200'>{pressedKey}</span>
-                    <span className='text-xl font-semibold text-yellow-400'>{beat}</span>
+                    <span className='text-xl md:text-3xl font-bold number text-shadow-sm text-shadow-amber-200'>{pressedKey}</span>
+                    <span className='text-[10px] md:text-lg font-semibold text-yellow-400 truncate w-full px-1'>{beat}</span>
                 </div>
             </button>
         </div>
