cb136b9fe300bbefafb69605f5038bcd6c7f1d6f
Anuj <anujnautiyal393@gmail.com>
2025-12-26 15:56:14 +0530

    Add frontend of the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,32 @@ function App() {
 
   return (
     <>
-      <h1 className='text-9xl bg-purple-500'>hello</h1>
+      <main className='App relative w-screen h-screen overflow-hidden'>
+        <div className='full-screen-bg fixed'></div>
+
+        <section className='hero relative flex justify-center items-center text-white border h-full w-full z-2'>
+          <div className='drum-machine-card flex flex-col items-center gap-40 w-[50vw] h-[80vh]'>
+            <div className='text-6xl font-bold my-3 border-2 border-red-400'>Kick & Snare</div>
+            <div className='grid grid-cols-3 border-2 border-blue-300 w-[80%] h-[50%]'>
+              <div className="first">
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+              </div>
+              <div className="second">
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+              </div>
+              <div className="third">
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+                <p className='h-[33%] border border-red-100'>1</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
