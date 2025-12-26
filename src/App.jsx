ca2b39ea95b4b813849d9a2591f492ba996b1fb4
Anuj <anujnautiyal393@gmail.com>
2025-12-26 23:52:49 +0530

    fix: move all the audios to public folder and change the path

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,19 @@ function App() {
 
             <div className='grid grid-cols-3 w-[95%] md:w-[80%] h-[60%] gap-2'>
               <div className="first flex flex-col justify-center md:justify-between h-full">
-                <Drumpad beat="Tom-1" pressedKey="7" audiosrc="src/assets/tom-1.mp3" />
-                <Drumpad beat="O-Hihat" pressedKey="4" audiosrc="src/assets/O-hihat.mp3" />
-                <Drumpad beat="C-Hihat" pressedKey="1" audiosrc="src/assets/C-hihat.mp3" />
+                <Drumpad beat="Tom-1" pressedKey="7" audiosrc="/sounds/tom-1.mp3" />
+                <Drumpad beat="O-Hihat" pressedKey="4" audiosrc="/sounds/O-hihat.mp3" />
+                <Drumpad beat="C-Hihat" pressedKey="1" audiosrc="/sounds/C-hihat.mp3" />
               </div>
               <div className="second flex flex-col justify-center md:justify-between h-full">
-                <Drumpad beat="C-Cymbal" pressedKey="8" audiosrc="src/assets/C-cymbal.mp3" />
-                <Drumpad beat="Snare" pressedKey="5" audiosrc="src/assets/snare.mp3" />
-                <Drumpad beat="Kick" pressedKey="2" audiosrc="src/assets/kick.mp3" />
+                <Drumpad beat="C-Cymbal" pressedKey="8" audiosrc="/sounds/C-cymbal.mp3" />
+                <Drumpad beat="Snare" pressedKey="5" audiosrc="/sounds/snare.mp3" />
+                <Drumpad beat="Kick" pressedKey="2" audiosrc="/sounds/kick.mp3" />
               </div>
               <div className="third flex flex-col justify-center md:justify-between h-full">
-                <Drumpad beat="Tom-2" pressedKey="9" audiosrc="src/assets/tom-2.mp3" />
-                <Drumpad beat="R-cymbal" pressedKey="6" audiosrc="src/assets/R-cymbal.mp3" />
-                <Drumpad beat="Floortom" pressedKey="3" audiosrc="src/assets/floortom.mp3" />
+                <Drumpad beat="Tom-2" pressedKey="9" audiosrc="/sounds/tom-2.mp3" />
+                <Drumpad beat="R-cymbal" pressedKey="6" audiosrc="/sounds/R-cymbal.mp3" />
+                <Drumpad beat="Floortom" pressedKey="3" audiosrc="/sounds/floortom.mp3" />
               </div>
             </div>
           </div>
